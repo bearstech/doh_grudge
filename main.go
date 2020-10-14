6974477564216834a3e1f9861cc06a661f299d2f
Mathieu Lecarme <mlecarme@bearstech.com>
2020-10-14 14:37:49 +0200

    Cache.

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/miekg/dns"
 )
@@ -12,11 +14,22 @@ import (
 func main() {
 
 	c := new(dns.Client)
+	cache := make(map[string][]byte)
+	fmt.Println(cache)
+	l := sync.RWMutex{}
 
 	http.HandleFunc("/dns-query", func(w http.ResponseWriter, r *http.Request) {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			panic(err)
+		}
+		encoded := base64.StdEncoding.EncodeToString(body)
+		l.RLock()
+		defer l.RUnlock()
+		cc, ok := cache[encoded]
+		if ok {
+			w.Write(cc)
+			return
 		}
 		m := new(dns.Msg)
 		err = m.Unpack(body)
@@ -32,6 +45,9 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+		l.Lock()
+		defer l.Unlock()
+		cache[encoded] = b
 		w.Write(b)
 	})
 
