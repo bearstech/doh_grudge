18203bb4e523c699267bab975fc00fd406a84a22
Mathieu Lecarme <mlecarme@bearstech.com>
2020-10-14 16:40:05 +0200

    Module.

---
+++ doh/doh.go
@@ -0,0 +1,71 @@
+package doh
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+
+	"github.com/miekg/dns"
+)
+
+type Server struct {
+	cache    map[string]*dns.Msg
+	lock     *sync.RWMutex
+	resolver string
+	client   *dns.Client
+}
+
+func New(resolver string) *Server {
+	return &Server{
+		cache:    make(map[string]*dns.Msg),
+		lock:     &sync.RWMutex{},
+		resolver: resolver,
+		client:   new(dns.Client),
+	}
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	m := new(dns.Msg)
+	err = m.Unpack(body)
+	if err != nil {
+		panic(err)
+	}
+	mm := m.Copy()
+	mm.Id = 0
+	k := mm.String()
+	fmt.Println(k)
+	s.lock.RLock()
+	cc, ok := s.cache[k]
+	if ok {
+		r := cc.Copy()
+		r.Id = m.Id
+		b, err := r.Pack()
+		if err != nil {
+			panic(err)
+		}
+		w.Write(b)
+		s.lock.RUnlock()
+		fmt.Println("Hit")
+		return
+	}
+	s.lock.RUnlock()
+	fmt.Println("Miss")
+	in, rtt, err := s.client.Exchange(m, s.resolver)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rtt)
+	b, err := in.Pack()
+	if err != nil {
+		panic(err)
+	}
+	s.lock.Lock()
+	s.cache[k] = in
+	s.lock.Unlock()
+	w.Write(b)
+}
--- main.go
+++ main.go
@@ -1,70 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"sync"
 
-	"github.com/miekg/dns"
+	"github.com/bearstech/doh_grudge/doh"
 )
 
 func main() {
 
-	c := new(dns.Client)
-	cache := make(map[string]*dns.Msg)
-	l := sync.RWMutex{}
 	resolver := os.Getenv("DNS")
 	if resolver == "" {
 		resolver = "1.1.1.1:53"
 	}
 
-	http.HandleFunc("/dns-query", func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			panic(err)
-		}
-		m := new(dns.Msg)
-		err = m.Unpack(body)
-		if err != nil {
-			panic(err)
-		}
-		mm := m.Copy()
-		mm.Id = 0
-		k := mm.String()
-		fmt.Println(k)
-		l.RLock()
-		cc, ok := cache[k]
-		if ok {
-			r := cc.Copy()
-			r.Id = m.Id
-			b, err := r.Pack()
-			if err != nil {
-				panic(err)
-			}
-			w.Write(b)
-			l.RUnlock()
-			fmt.Println("Hit")
-			return
-		}
-		l.RUnlock()
-		fmt.Println("Miss")
-		in, rtt, err := c.Exchange(m, resolver)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(rtt)
-		b, err := in.Pack()
-		if err != nil {
-			panic(err)
-		}
-		l.Lock()
-		cache[k] = in
-		l.Unlock()
-		w.Write(b)
-	})
+	s := doh.New(resolver)
 
+	http.Handle("/dns-query", s)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
